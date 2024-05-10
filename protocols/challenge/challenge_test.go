4b986f37f72dc88a01d07642279dba039084e823
neeraj <neeraj.rtly@gmail.com>
2024-05-10 10:27:36 +0530

    Create challenge protocol test

---
+++ protocols/challenge/challenge_test.go
@@ -0,0 +1,21 @@
+package challenge
+
+import (
+	"testing"
+)
+
+func TestChallenge(t *testing.T) {
+	t.Log("Test challenge protocol")
+
+	// Start the chain & deploy contract
+
+	// Create two go-nitro node
+
+	// Create ledger channel
+
+	// Close the node B
+
+	// Node A call challenge method and wait for challenge duration
+
+	// Node A call transfer method and check assets are liquidated
+}
