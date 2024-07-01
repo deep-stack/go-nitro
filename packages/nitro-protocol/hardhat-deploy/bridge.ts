71b7ed19a71b6802c54029d77170bc20703b0963
neeraj <neeraj.rtly@gmail.com>
2024-07-01 18:23:44 +0530

    Add script to deploy bridge contract

---
+++ packages/nitro-protocol/hardhat-deploy/bridge.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {deployments, getNamedAccounts, getChainId, network} = hre;
+  const {deploy} = deployments;
+
+  const {deployer} = await getNamedAccounts();
+
+  const addressesFilePath = `hardhat-deployments/${network.name}/.contracts.env`;
+  let contractAddresses = '';
+
+  console.log('Working on chain id #', await getChainId());
+  console.log('deployer', deployer);
+
+  const deployResult = await deploy('Bridge', {
+    from: deployer,
+    log: true,
+    deterministicDeployment: true,
+  });
+
+  contractAddresses = `${contractAddresses}export BRIDGE_ADDRESS=${deployResult.address}\n`;
+  const outputFilePath = path.resolve(addressesFilePath);
+  fs.writeFileSync(outputFilePath, contractAddresses);
+  console.log('Contracts deployed, addresses written to', outputFilePath);
+};
+export default func;
+func.tags = ['bridge'];
