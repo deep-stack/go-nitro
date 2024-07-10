e1deba856cf11ca535131b552208ce783cad1f2a
neeraj <neeraj.rtly@gmail.com>
2024-07-10 20:02:35 +0530

    Support commonjs imports in vite apps

--- packages/nitro-gui/vite.config.ts
+++ packages/nitro-gui/vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies
+  optimizeDeps: {
+    include:  ["@statechannels/nitro-rpc-client"]
+  },
+  build: {
+    commonjsOptions: {
+      include: [/nitro-rpc-client/, /node_modules/],
+    },
+  }
 });
