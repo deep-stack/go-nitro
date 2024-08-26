80617744c7c81684db60475d1d7c0798fd047b20
Nabarun Gogoi <nabarun@deepstacksoft.com>
2024-08-26 14:49:54 +0530

    Bridge L1 node rejects objective creation based on policy maker (#84)
    Co-authored-by: Neeraj <neeraj.rtly@gmail.com>

---
+++ bridge/policy_maker.go
@@ -0,0 +1,19 @@
+package bridge
+
+import (
+	"github.com/statechannels/go-nitro/protocols"
+	"github.com/statechannels/go-nitro/protocols/directdefund"
+	"github.com/statechannels/go-nitro/protocols/virtualdefund"
+	"github.com/statechannels/go-nitro/protocols/virtualfund"
+)
+
+type NodeL1PermissivePolicy struct{}
+
+func (pp *NodeL1PermissivePolicy) ShouldApprove(o protocols.Objective) bool {
+	// L1 node rejects objectives if they involve virtual funding, virtual defunding, or direct defunding
+	if virtualfund.IsVirtualFundObjective(o.Id()) || virtualdefund.IsVirtualDefundObjective(o.Id()) || directdefund.IsDirectDefundObjective(o.Id()) {
+		return false
+	}
+
+	return o.GetStatus() == protocols.Unapproved
+}
